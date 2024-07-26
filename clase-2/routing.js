48bb8c5269e2a166fbb94d4be7637e443ef8131c
adriarce99 <adrianarcexd@gmail.com>
2024-07-26 19:47:13 +0200

    Finalización clase de protocolos

--- clase-2/routing.js
+++ clase-2/routing.js
@@ -10,17 +10,36 @@ const processRequest = (req, res) => {
   switch (method) {
     case 'GET':
       switch (url) {
-        case '/pokemon/ditto':
+        case '/pokemon/ditto': {
           res.setHeader('Content-Type', 'aplication/json; charset=utf-8')
           return res.end(JSON.stringify(dittoJSON))
-        default:
+        }
+        default: {
           res.setHeader('Content.Type', 'text/html; charset=utf-8')
           return res.end('<h1>404</h1>')
+        }
       }
     case 'POST':
       switch (url) {
-        case '/pokemon':
-          const body = ''
+        case '/pokemon': {
+          let body = ''
+          // Escuchando data
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // Aqui podria llamar a una base de datos
+            // Otra forma de escribir el header
+            res.writeHead(201, { 'Content-Type': 'aplication/json; charset=utf-8' })
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+        default: {
+          res.setHeader('Content.Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404</h1>')
+        }
       }
   }
 }
